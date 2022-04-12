1981effb6a7d6d8c26313987ca69860d0ba5c65e
John Doe <hokkibolta@tozya.com>
2022-04-12 16:15:06 +0200

    add ethers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import HomePage from "./pages/HomePage";
+import { ethers } from "ethers";
 
 function App() {
+  //const []: [] = ;
+
+  const connectProvider = async () => {
+    await (window as any).ethereum.enable()
+    const provider = new ethers.providers.Web3Provider(
+      (window as any).ethereum
+    );
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+  };
+
+  useEffect(() => {
+    connectProvider().then(() => {
+      console.log("metamask connected");
+    });
+  }, []);
   return (
     <div className="App">
       <HomePage />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
 const HomePage = () => {
   return (
     <div className="container-fluid">
